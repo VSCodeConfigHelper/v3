8d4f125ded871be0131ffdbde7397ce1978832f5
Guyutongxue <guyutongxue@163.com>
2021-08-05 11:04:30 +0800

    add license header

--- pages/config.js
+++ pages/config.js
@@ -1,19 +1,19 @@
 // Copyright (C) 2021 Guyutongxue
 // 
-// This file is part of VSCodeConfigHelper.
+// This file is part of VS Code Config Helper.
 // 
-// VSCodeConfigHelper is free software: you can redistribute it and/or modify
+// VS Code Config Helper is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 // 
-// VSCodeConfigHelper is distributed in the hope that it will be useful,
+// VS Code Config Helper is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU General Public License
-// along with VSCodeConfigHelper.  If not, see <http://www.gnu.org/licenses/>.
+// along with VS Code Config Helper.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
  * Get the directory name of path
