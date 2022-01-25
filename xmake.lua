bf611715697d26cb9d15e3807dc7fa601ca6223d
Guyutongxue <guyutongxue@163.com>
2022-01-25 16:00:17 +0800

    package

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,9 @@
-local BOOST_FLAGS = "-D_WIN32_WINNT=0x0600 -DBOOST_USE_WINAPI_VERSION=0x0600 -DBOOST_USE_WINDOWS_H"
+set_xmakever("2.6.3")
 
+add_rules("mode.debug", "mode.minsizerel")
+
+-- Boost.Log requires correct WINAPI version
+local BOOST_FLAGS = "-D_WIN32_WINNT=0x0600 -DBOOST_USE_WINAPI_VERSION=0x0600 -DBOOST_USE_WINDOWS_H"
 add_requires("boost 1.78.0", {
     configs = {
         algorithm = true,
@@ -13,29 +17,25 @@ add_requires("boost 1.78.0", {
     }
 })
 add_requires("cpp-httplib 0.9.2", {
-    configs = {
-        ssl = true
-    }
+    configs = { ssl = true }
 })
 add_requires("nlohmann_json 3.10.0")
-
-add_rules("mode.debug", "mode.minsizerel")
 
 target("vscch3")
     set_version("3.1.5")
     set_languages("cxx20")
     add_files("src/*.cpp")
     add_packages("boost", "cpp-httplib", "nlohmann_json")
-    -- Use "Unicode" Win32 API
-    add_defines("UNICODE", "_UNICODE")
-    -- required by nlohmann_json
-    add_defines("JSON_USE_IMPLICIT_CONVERSIONS")
-    -- required by cpp-httplib
-    add_defines("CPPHTTPLIB_OPENSSL_SUPPORT")
-    -- required by Boost.Nowide, Boost.Process
-    -- https://github.com/boostorg/process/issues/96
-    add_defines("WIN32_LEAN_AND_MEAN")
-    add_cxflags(BOOST_FLAGS)
+    set_targetdir("$(buildir)/bin")
+    add_defines(
+        "UNICODE",                        -- Use "Unicode" Win32 API
+        "_UNICODE",
+        "JSON_USE_IMPLICIT_CONVERSIONS",  -- required by nlohmann_json
+        "CPPHTTPLIB_OPENSSL_SUPPORT",     -- required by cpp-httplib
+        "WIN32_LEAN_AND_MEAN"             -- required by Boost.Nowide, Boost.Process
+                                          -- https://github.com/boostorg/process/issues/96
+    )
+    add_cxflags(BOOST_FLAGS)              -- Use same flags as building Boost
     if is_plat("windows") then
         add_files("configs/resource.rc")
         -- https://github.com/xmake-io/xmake/issues/2008
@@ -43,24 +43,22 @@ target("vscch3")
         add_ldflags("/MANIFESTINPUT:configs/app.manifest", { force = true })
         add_cxflags("/utf-8")
         add_links("shell32", "advapi32", "ole32")
+        set_basename("VSCodeConfigHelper")
     elseif is_plat("linux") then
         add_cxflags("-static-libgcc", "-static-libstdc++")
     elseif is_plat("macosx") then
         add_cxflags("-static")
     end
-    ---------------------
-    -- Set config file --
-    ---------------------
+    
+    -- Set config file
     set_configvar("DEFAULT_GUI_ADDRESS", "https://guyutongxue.gitee.io/vscodeconfighelper3/config.html")
     on_load(function (target)
         -- https://github.com/xmake-io/xmake/discussions/2006#discussioncomment-2034133
         function set_from_file(var, source)
-            local content = io.readfile(source)
-            content = content:replace("$(", "%$(", { plain = true })
+            local content = io.readfile(source):replace("$(", "%$(", { plain = true })
             -- Add UTF-8 BOM for Windows scripts
             if source:endswith(".ps1") then
-                local BOM = "\xef\xbb\xbf"
-                content = BOM .. content
+                content = "\xef\xbb\xbf" .. content
             end
             target:set("configvar", var, content)
         end
@@ -77,4 +75,15 @@ target("vscch3")
     set_configdir("$(buildir)/include")
     add_includedirs("$(buildir)/include")
     add_configfiles("configs/config.h.in", { pattern = "@(.-)@" })
+
+    -- Create 7z archive
+    on_package(function (target)
+        os.rm("$(buildir)/package")
+        os.run(
+            "7z a " ..
+            "$(buildir)/package/VSCodeConfigHelper_v" .. target:get("version") .. ".7z " ..
+            "./$(buildir)/bin/*",
+            "LICENSE"
+        )
+    end)
 target_end()
